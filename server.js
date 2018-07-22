4a27529ab17456a9a11f4befd151ef01de2d04b4
auztin410 <auztin410@gmail.com>
2018-07-22 15:20:05 -0400

    Added logic for variable surveryResults to select which page is shown.

--- server.js
+++ server.js
@@ -8,12 +8,29 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-
+var surveryResults = 0;
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: http://localhost:" + PORT);
 });
 
 app.post("/characters", function(req, res){
-
-})
+    if(surveryResults >= 30){
+        res.sendFile(path.join(__dirname, "public/morty.html"));
+    }
+    else if(surveryResults >= 26){
+        res.sendFile(path.join(__dirname, "public/birdperson.html"));
+    }
+    else if(surveryResults >= 22){
+        res.sendFile(path.join(__dirname, "public/squanchy"));
+    }
+    else if(surveryResults >= 18){
+        res.sendFile(path.join(__dirname, "public/beth,html"));
+    }
+    else if(surveryResults >= 14){
+        res.sendFile(path.join(__dirname, "public/summer.html"));
+    }
+    else{
+        res.sendFile(path.join(__dirname, "public/jerry.html"));
+    }
+});
